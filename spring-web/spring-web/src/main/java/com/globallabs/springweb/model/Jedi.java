c456a79afddfb04ccf3eb67d039708f426aa6cda
perkles <anderson.os88@hotmail.com>
2021-10-11 12:26:08 -0300

    spring web-mvc

---
+++ spring-web/spring-web/src/main/java/com/globallabs/springweb/model/Jedi.java
@@ -0,0 +1,33 @@
+package com.globallabs.springweb.model;
+
+public class Jedi {
+
+
+    private String name;
+    private String lastName;
+
+    public Jedi(final String name, final String lastname) {
+        this.name = name;
+        this.lastName = lastname;
+    }
+
+    public Jedi(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
